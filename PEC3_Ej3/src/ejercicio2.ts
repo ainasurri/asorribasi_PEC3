e1a5bd5044337e818b7455cc86e3af0504dd02ab
AinaSorribas <122831540+AinaSorribas@users.noreply.github.com>
2023-11-19 18:46:51 +0100

    ex3_c

---
+++ PEC3_Ej3/src/ejercicio2.ts
@@ -0,0 +1,36 @@
+// Sustituye /***/ por las instrucciones adecuadas que cumplan las operaciones 
+// y salidas indicadas en los comentarios.
+
+
+interface Plane{
+    model:string,
+    npassengers:number
+}
+
+interface HangarHash {
+    [key: string]: Plane
+}
+
+let myHangar:HangarHash = {}
+
+myHangar['123Z']={
+    model:'airbus',
+    npassengers:200
+}
+myHangar['H789']={ 
+    model:'boeing',
+    npassengers:151
+}
+
+/** Print following lines (going through the object)
+ * 123Z:airbus(200)
+ * H789:boeing(151)
+ */
+
+function printHangarDetails(hangar: HangarHash): void {
+    for (const key in hangar) {
+        const plane = hangar[key];
+        console.log(`${key}:${plane.model}(${plane.npassengers})`);
+    }
+}
+printHangarDetails(myHangar);
